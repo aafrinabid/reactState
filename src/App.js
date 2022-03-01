57ea84efd3678e3c32820fa80b5483ed22f22163
aafrinabid <mohdaafrin@gmail.com>
2022-03-01 20:04:32 +0530

    increment learning

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Message from './components/message'
+import Counter from './components/counter'
 function App() {
   return (
     <div className="App">
-    <Message />
+    {/* <Message /> */}
+    <Counter />
     </div>
   );
 }
